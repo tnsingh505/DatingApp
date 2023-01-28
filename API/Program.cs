c5a1dff7d07e7ced9077c563a5bfebebf26bc998
Tanya Singh <tnsingh505@gmail.com>
2023-01-28 22:29:52 +0530

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,18 +4,19 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
---
+++ client/src/app/-- SQLite.sql
@@ -0,0 +1,7 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Rupal");
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Tanya");
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Tara");
